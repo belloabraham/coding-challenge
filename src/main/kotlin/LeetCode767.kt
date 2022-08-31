27ac6984dfdcbf31a7cbe96d533807e95a29cb73
Bello Abraham <belloabraham101@gmail.com>
2022-08-31 11:10:20 -0500

    Added LeetCode767.kt

---
+++ src/main/kotlin/LeetCode767.kt
@@ -0,0 +1,38 @@
+import java.util.*
+
+fun reOrganizeString(value:String): String {
+
+    if(value.isBlank())
+        return ""
+
+    val repeatQueue:Queue<Char> = LinkedList()
+    val wordQueue:Queue<Char> = LinkedList()
+    for (char in value)
+        wordQueue.add(char)
+
+    var orgWord = ""
+    while (wordQueue.isNotEmpty()){
+
+        if(repeatQueue.isNotEmpty() &&  !orgWord.endsWith(repeatQueue.peek())){
+            orgWord += repeatQueue.remove()
+        }else if(!orgWord.endsWith(wordQueue.peek())){
+            orgWord += wordQueue.remove()
+        }else{
+            repeatQueue.add(wordQueue.remove())
+        }
+    }
+
+
+    if(repeatQueue.size > 1){
+        return  ""
+    }else if(repeatQueue.size == 1){
+        val last = repeatQueue.remove()
+        return if (orgWord.endsWith(last)){
+            ""
+        }else {
+            orgWord.plus(last)
+        }
+    }
+
+    return orgWord
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,6 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-   val result = mostCommonWord("Bob hit a ball, the hit ball flew far after it was hit.", arrayOf("hit"))
-    println(result)
+    println(reOrganizeString("abaab"))
 
 }
