14aa054aeb7ba0fd607f928bd20eb00ccd948813
Bello Abraham <belloabraham101@gmail.com>
2022-09-01 08:16:11 -0500

    Added LeetCode1221.kt

--- src/main/kotlin/LeetCode1221.kt
+++ src/main/kotlin/LeetCode1221.kt
@@ -1,18 +1,18 @@
 
 fun splitBalancedString(value:String): Int {
 
-    var string = ""
+    val string = StringBuilder()
     var sumOfBalStrong = 0
 
     for(item in value){
-        string += item
+        string.append(item)
         if(string.length % 2 == 0) {
                 val start = string.subSequence(0 until (string.length/2))
                 val end = string.takeLast(string.length/2)
 
                 if(start[0] != end[end.length -1]){
                     sumOfBalStrong++
-                    string = ""
+                    string.clear()
                 }
         }
     }
